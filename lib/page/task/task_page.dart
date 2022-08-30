aface5a3b4cd6011c3940fd929392caa860ebf04
anhhieu21 <nhuanhhieu21@gmail.com>
2022-08-30 03:54:23 -0700

    Add gridview for task page

--- lib/page/task/task_page.dart
+++ lib/page/task/task_page.dart
@@ -4,23 +4,56 @@ import 'package:smartwork/cubit/task_cubit.dart';
 import 'package:smartwork/models/task.dart';
 import 'package:smartwork/styles/colors.dart';
 
+const _kMaxCrossAxisExtent = 350.0;
+const _kChildAspectRatio = 1.0;
+
 class TaskPage extends StatelessWidget {
   const TaskPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final size = MediaQuery.of(context).size;
+    // int count = (size.width / _kMaxCrossAxisExtent).ceil();
+    // final width = (size.width / count) - 24;
+
     return Scaffold(
       backgroundColor: colorBgPage,
       body: BlocBuilder<TaskCubit, TaskState>(
           bloc: BlocProvider.of<TaskCubit>(context)..getListTask(),
           builder: (context, state) {
             if (state is TaskData) {
-              return ListView.builder(
-                  itemCount: state.list.length,
-                  itemBuilder: (context, index) {
-                    final item = state.list[index];
-                    return Text(item.title);
-                  });
+              return GridView.extent(
+                maxCrossAxisExtent: _kMaxCrossAxisExtent,
+                childAspectRatio: _kChildAspectRatio,
+                physics: const ScrollPhysics(),
+                children: List.generate(state.list.length, (index) {
+                  final item = state.list[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            color: colorWhite,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: colorShadown,
+                                offset: Offset(5, 8),
+                                blurRadius: 8.0,
+                              )
+                            ]),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(item.title),
+                            Text(item.content),
+                            Text(item.createdAt),
+                            Text(item.tag),
+                          ],
+                        )),
+                  );
+                }),
+              );
             } else {
               return const Center(
                 child: Text('Empty'),
--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -4,3 +4,4 @@ const colorBgPage = Color.fromARGB(255, 246, 246, 246);
 const colorItemNav = Color.fromARGB(255, 248, 154, 246);
 const colorWhite = Colors.white;
 const colorBlack = Colors.black;
+const colorShadown = Color.fromARGB(255, 240, 238, 238);
